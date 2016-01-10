536fa1c7ad941c0552c0cf0e8c72ddb297af3c95
ArthurHlt <arthurh.halet@gmail.com>
2016-01-10 09:32:31 +0100

    use tts-watson python package

--- scripts/tts.py
+++ scripts/tts.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 import rospy
+import os
 from std_msgs.msg import String
-from tts_watson.PlayerTts import PlayerTts
+from tts_watson.TtsWatson import TtsWatson
+import anyconfig
+import bunch
 
-class TtsWatson:
-	def __init__(self):
-		self.playerTts = PlayerTts()
-	def playSound(self, data):
-		self.playerTts.play(data.data)
+
+class TtsWatsonRos:
+    CONFIG_FILE = "config.yml"
+
+    def __init__(self):
+        currentDir = os.path.dirname(os.path.realpath(__file__))
+        configFile = currentDir + "/../" + self.CONFIG_FILE
+        conf = anyconfig.load(configFile)
+        bconf = bunch.bunchify(conf)
+        self.ttsWatson = TtsWatson(bconf.user, bconf.password, bconf.voice, bconf.url, bconf.chunk)
+
+    def playSound(self, data):
+        self.ttsWatson.play(data.data)
+
 
 def listen():
-	ttsWatson = TtsWatson()
-	rospy.init_node('listener', anonymous=True)
-	rospy.Subscriber("text_to_speech", String, ttsWatson.playSound)
-	print 'Ready to transform text into sound'
-	# spin() simply keeps python from exiting until this node is stopped
-	rospy.spin()
+    ttsWatsonRos = TtsWatsonRos()
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("text_to_speech", String, ttsWatsonRos.playSound)
+    print 'Ready to transform text into sound'
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
 if __name__ == '__main__':
     listen()
-    
